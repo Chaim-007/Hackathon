84ede24ddae7807c69563b59d0ff24af3643728e
Chaitanya Gupta <mailchaim007@gmail.com>
2022-08-28 23:02:49 +0530

    submitting

--- routes/apply.js
+++ routes/apply.js
@@ -1,7 +1,7 @@
 const router = require('express').Router();
 const {isAuthorized, isVerified} = require('../config/authCheck');
 const Users = require('../models/Users');
-const Posts = require('../models/Posts');
+const Register = require('../models/Register');
 
 router.get('/', isAuthorized, isVerified, (req,res)=>{
     res.render('register');
